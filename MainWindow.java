bb514a4dc455168f91d619e9766dcdccabfab1b4
Tyler Moll <tymo4210@colorado.edu>
2020-10-31 23:38:33 -0600

    initial commit

---
+++ MainWindow.java
@@ -0,0 +1,54 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+public class MainWindow {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MainWindow window = new MainWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setImage(SWTResourceManager.getImage("C:\\Users\\Tyler Moll\\Desktop\\Glove Mouse\\cheese.jpg"));
+		shell.setSize(520, 519);
+		shell.setText("SWT Application");
+		
+		Menu menu = new Menu(shell, SWT.BAR);
+		shell.setMenuBar(menu);
+
+	}
+}
